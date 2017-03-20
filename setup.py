0ab4e77febfb00c40d773c2606a9a2849cd18522
Bryce Guinta <bryce.paul.guinta@gmail.com>
2017-03-20 16:10:46 -0600

    Make setup script easier to understand.

--- setup.py
+++ setup.py
@@ -10,14 +10,25 @@ Setup all symlinks to home directory for these dotfiles.
 This file assumes it's in the base directory if dotfiles
 """
 
-from collections import OrderedDict
-import filecmp
-import os
-import subprocess
-import sys
+from itertools import count
+from os.path import exists, join, islink, isabs, dirname, realpath
+from os import unlink, readlink, symlink, environ
 import shutil
+try:
+    # Use generator filter for python 2
+    # pylint: disable=redefined-builtin,ungrouped-imports
+    from itertools import ifilter as filter  # type: ignore
+except ImportError:
+    pass
+try:
+    # Ignore typing if not installed
+    # pylint: disable=unused-import
+    from typing import (  # noqa: F401
+        Tuple, Callable, Iterable, Optional, TypeVar)
+except ImportError:
+    pass
 
-DOTFILE_DIR = os.path.dirname(os.path.realpath(__file__))
+DOTFILE_DIR = dirname(realpath(__file__))
 
 # Change this to configure the symlink mapping
 DOTFILE_TO_HOME = (
@@ -31,55 +42,156 @@ DOTFILE_TO_HOME = (
 )
 
 
+def main(symlink_pair, cur_dir):
+    # type: (Tuple[Tuple[str, str], ...], str) -> None
+    """Evaluate tuple pairs as symlinks from home pointing to the dotfiles.
+
+    Args:
+        symlink_pair: A tuple of tuple pairs containing relative paths.
+            The first entry of a tuple contains a path relative to the
+            dotfile location, and the second entry contains a path relative
+            to the home directory. The location at the home directory will
+            be symlinked to point to the location in the dotfile directory
+        cur_dir: An absolute directory path which points to the
+            location of the dotfiles..
+        """
+    for src, dst in symlink_pair:
+        src_full_path = join(cur_dir, src)
+        dst_full_path = join(environ['HOME'], dst)
+        link_pair(src_full_path, dst_full_path)
+    print("Linking is complete.")
+
+
+def link_pair(src, dst):
+    # type: (str, str) -> None
+    """Point src path to dst path if possible."""
+    should_continue = handle_symlink_case(dst, src)
+    if not should_continue:
+        return
+    handle_file_backup(dst)
+    print("{} => {}".format(dst, src))
+    symlink(src, dst)
+
+
+def handle_symlink_case(dst, src):
+    # type: (str, str) -> bool
+    """Handle checking for symlinks at the destination location.
+
+    Returns:
+        False to not continue linking
+        True to continue linking location
+    """
+    if islink(dst):
+        if is_bad_symlink(dst):
+            print("Broken symlink found at {}. Removing"
+                  .format(dst))
+            unlink(dst)
+        elif src == readlink(dst):
+            print("{} is already at source".format(src))
+            return False
+    return True
+
+
+def is_bad_symlink(filepath):
+    # type: (str) -> bool
+    """Returns True if symlink is relative or the file its
+    pointing to does not exist."""
+    is_symlink = islink(filepath)
+    if not is_symlink:
+        return False
+    symlink_dst_exists = exists(readlink(filepath))
+    abs_symlink = isabs(readlink(filepath))
+    return not (symlink_dst_exists and abs_symlink)
+
+
+def handle_file_backup(filepath):
+    # type: (str) -> None
+    """Moving existing file to a backup location."""
+    if not exists(filepath):
+        return
+    bak_path = get_bak_path(filepath)
+    print("Backing up existing file '{}' to '{}'"
+          .format(filepath, bak_path))
+    shutil.move(filepath, bak_path)
+
+
 def get_bak_path(filepath):
+    # type: (str) -> str
     """Get a backup filepath that does not exist.
-    
+
     Use filepath to generate a path that is filepath.bak
     If it already exists, then generate a path such as filepath.bak1
     and so on.
     """
+    candidate_paths = candidate_bak_path_retrieval(filepath)
+    return first_true(candidate_paths, pred=path_available)
+
+
+def path_available(filepath):
+    # type: (str) -> bool
+    """Return true if filepath is available"""
+    parent_directory = dirname(filepath)
+    if not exists(parent_directory):
+        raise ParentDirectoryDoesNotExist(parent_directory, filepath)
+    return not exists(filepath)
+
+
+class ParentDirectoryDoesNotExist(Exception):
+    """Exception for when a parent directory does not exist."""
+    def __init__(self, parent_directory, filepath):
+        self.parent_directory = parent_directory
+        self.filepath = filepath
+        # This is right pylint
+        # pylint: disable=bad-super-call
+        super(Exception, self).__init__(str(self))
+
+    def __str__(self):
+        fmt = "filepath '{filepath}' in '{parent_directory}'"
+        return fmt.format(
+            filepath=self.filepath,
+            parent_directory=self.parent_directory)
+
+
+try:
+    T = TypeVar('T')
+except NameError:
+    pass
+
+
+def first_true(iterable, pred=None):
+    # type: (Iterable[T], Optional[Callable]) -> T
+    """Return the first true iterable.
+
+    If pred is not None, then apply predicate to iterable before checking
+    truth value.
+    """
+    try:
+        return next(filter(pred, iterable))
+    except StopIteration:
+        raise NoTrueValueError
+
+
+class NoTrueValueError(ValueError):
+    """Exception for when there is no true value."""
+    pass
+
+
+def candidate_bak_path_retrieval(filepath):
+    # type: (str) -> Iterable[str]
+    """Yield candidate path for backup file."""
     ext = ".bak"
     template_new_path = "{}{}".format(filepath, ext)
-    counter = 1
-    new_path = template_new_path
-    while os.path.exists(new_path):
-        new_path = "{}{}".format(template_new_path, counter)
-        counter += 1
-    return new_path
+    for ext_append in get_ext_append():
+        yield "{}{}".format(template_new_path, ext_append)
 
 
-def is_bad_symlink(filepath):
-    """Returns True if symlink is relative or the file its
-    pointing to does not exist."""
-    is_symlink = os.path.islink(filepath)
-    if not is_symlink:
-        return False
-    symlink_dst_exists = os.path.exists(os.readlink(filepath))
-    abs_symlink = os.path.isabs(os.readlink(filepath))
-    return not (symlink_dst_exists and abs_symlink)
+def get_ext_append():
+    # type: () -> Iterable[str]
+    """Yield the extension for the current number."""
+    yield ""
+    for num in count(start=1):
+        yield str(num)
 
-
-def main(symlink_pair, cur_dir):
-    """Evaluate all tuples as symlinks from cur_dir to the home dir."""
-    for src, dst in symlink_pair:
-        src_full_path = os.path.join(cur_dir, src)
-        dst_full_path = os.path.join(os.environ['HOME'], dst)
-        if os.path.islink(dst_full_path):
-            if is_bad_symlink(dst_full_path):
-                print("Broken symlink found at {}. Removing"
-                      .format(dst_full_path))
-                os.unlink(dst_full_path)
-            elif src_full_path == os.readlink(dst_full_path):
-                print("{} is already at source".format(src))
-                continue
-        if os.path.exists(dst_full_path):
-            bak_path = get_bak_path(dst_full_path)
-            print("Backing up existing file '{}' to '{}'"
-                  .format(dst_full_path, bak_path))
-            shutil.move(dst_full_path, bak_path)
-        print("{} => {}".format(dst_full_path, src_full_path))
-        os.symlink(src_full_path, dst_full_path)
-    print("Linking is complete.")
 
 if __name__ == "__main__":
     main(DOTFILE_TO_HOME, DOTFILE_DIR)
