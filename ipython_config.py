a6e6e6bf18c48638d4c6c7d97f894edd3fc3c1ad
Bryce Guinta <bryce.paul.guinta@gmail.com>
2018-07-19 14:28:36 -0600

    Set default shell editor for ipython to gvim

--- ipython_config.py
+++ ipython_config.py
@@ -5,3 +5,4 @@ c.InteractiveShellApp.exec_lines.append(
 # Automatically reload modules
 c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
 c.InteractiveShellApp.exec_lines.append('%autoreload 2')
+c.TerminalInteractiveShell.editor = 'gvim'
