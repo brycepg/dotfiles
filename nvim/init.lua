97bd0b554a44c46b7ec019f81e48a8a9d2ad75fa
Bryce Guinta <bryce.guinta@protonmail.com>
2023-01-20 13:24:50 -0700

    [vim] do not commit openai key

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,16 @@
 local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim" vim.cmd.source(vimrc)
-require('neural').setup({
-    open_ai = {
-        api_key = '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'
-    }
-})
+
+-- API key required for :Neuralprompt to work
+local env = vim.fn.expand('$HOME') .. '/.config/nvim/env.lua'
+if vim.env.OPENAI_API_KEY == nil then
+    print("Please set the OPENAI_API_KEY environment variable")
+    return
+end
+local status_neural, neural = pcall(require, 'neural')
+if status_neural then
+    require('neural').setup({
+        open_ai = {
+            api_key = vim.env.OPEN_API_KEY
+        }
+    })
+end
