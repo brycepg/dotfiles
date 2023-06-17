98d539f9ed4caf4f246bc9f239b6d96f5f2cbaf8
Bryce Guinta <bryce.guinta@protonmail.com>
2023-06-17 11:18:13 -0600

    Make Neuralprompt slightly more optional in vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,18 @@
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
+
 -- MOTD: 'gf' to goto file
 local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim" vim.cmd.source(vimrc)
 
 -- API key required for :Neuralprompt to work
 local env = vim.fn.expand('$HOME') .. '/.vim/env.lua'
-dofile(env)
+if file_exists(env) then
+    dofile(env)
+end
 local status_neural, neural = pcall(require, 'neural')
 if status_neural then
-    if vim.env.OPENAI_API_KEY == nil then
-        print("Please set the OPENAI_API_KEY environment variable")
-    end
     require('neural').setup({
         open_ai = {
             api_key = vim.env.OPENAI_API_KEY
